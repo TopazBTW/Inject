b6c5878a55a9d2858611d0fdddcfb7da7bda195c
Topaz <Berserkmj23@gmail.com>
2025-05-18 02:19:52 +0100

    V1-FUNCTIONAL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
+import { PatientFormComponent } from './components/patient-form/patient-form.component';
+import { PatientDetailComponent } from './components/patient-detail/patient-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/patients', pathMatch: 'full' },
+  { path: 'patients', component: PatientListComponent },
+  { path: 'patients/new', component: PatientFormComponent },
+  { path: 'patients/edit/:id', component: PatientFormComponent },
+  { path: 'patients/:id', component: PatientDetailComponent },
+  { path: '**', redirectTo: '/patients' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent,
+    PatientFormComponent,
+    PatientDetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
